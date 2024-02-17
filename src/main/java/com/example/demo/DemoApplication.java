ee7456e0686974517cf17b04a28dea569b228db8
callmeliow <zhdeveloper0605@gmail.com>
2024-02-17 17:37:14 +0800

    update root path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	@GetMapping("/welcome")
+	@GetMapping("/")
 	public String welcome(){
 		return "Welcome";
 	}
